cc87e094f646cd46d9eb275c1b849cd703765f97
palanisamy2 <palanisamy20044@gmail.com>
2025-09-26 17:47:51 +0530

    update the backend connectivity

--- server/ewaste-reporting/config/database.js
+++ server/ewaste-reporting/config/database.js
@@ -1,13 +1,16 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // necessary for Supabase
+  },
 });
 
 pool.on('connect', () => {
-    console.log('Connected to the database successfully!');
+  console.log('Connected to Supabase successfully!');
 });
 
 module.exports = {
-    query: (text, params) => pool.query(text, params),
+  query: (text, params) => pool.query(text, params),
 };
